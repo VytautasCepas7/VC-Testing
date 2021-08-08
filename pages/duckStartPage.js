dcb6aa08c55376e368253a54a4ff6767e25bfbc4
VytautasCepas7 <88025765+VytautasCepas7@users.noreply.github.com>
2021-08-08 19:55:09 +0300

    Update duckStartPage.js
    v1

--- pages/duckStartPage.js
+++ pages/duckStartPage.js
@@ -2,13 +2,8 @@ exports.DuckStartPage = class DuckStartPage {
     constructor(page) {
         this.page = page;
     }
-    async goto() {
-    await this.page.goto('https://start.duckduckgo.com/');
-    }
 
-    async initiateSearch(searchCriteria) {
-    await page.waitForSelector('#logo_homepage_link');
-    await page.fill('#search_form_input_homepage', searchCriteria);
-    await page.click('#search_button_homepage');
+    async goto() {
+    await this.page.goto('https://testsheepnz.github.io/BasicCalculator');
     }
 }
